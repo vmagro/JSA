fdf2c88cd182aa4aabd5eafe52a7ecb2c191db71
vmagro <vmagro@socaldevs.com>
2012-09-21 15:38:21 -0700

    API for native apps

---
+++ src/org/jsa/socal/mobile/api/ConventionsApi.java
@@ -0,0 +1,53 @@
+package org.jsa.socal.mobile.api;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsa.socal.mobile.Convention;
+
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+
+@SuppressWarnings("serial")
+public class ConventionsApi extends HttpServlet {
+	
+	// GET is to retrieve information
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		String idString = req.getParameter("id");
+		if(idString == null){ //output all the conventions
+			ArrayList<Convention> conventions = Convention.getConventions();
+			JSONArray arr = new JSONArray();
+			for(Convention c : conventions){
+				JSONObject json = new JSONObject();
+				try {
+					json.put("id", c.getId());
+					json.put("title", c.getTitle());
+					json.put("loc", c.getLocation());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				arr.put(json);
+			}
+			resp.getWriter().println(arr);
+		}
+		else{
+			int id = Integer.parseInt(idString);
+			Convention c = Convention.getConvention(id);
+			JSONObject json = new JSONObject();
+			try {
+				json.put("id", c.getId());
+				json.put("title", c.getTitle());
+				json.put("loc", c.getLocation());
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			resp.getWriter().println(json);
+		}
+	}
+	
+}
---
+++ src/org/jsa/socal/mobile/api/DebateApi.java
@@ -0,0 +1,38 @@
+package org.jsa.socal.mobile.api;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsa.socal.mobile.Debate;
+
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+
+@SuppressWarnings("serial")
+public class DebateApi extends HttpServlet {
+	
+	// GET is used to retrieve info about the debate
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		JSONObject json = new JSONObject();
+		try{
+			int id = Integer.parseInt(req.getParameter("id"));
+			json.put("id", id);
+			Debate d = Debate.getDebate(id);
+			json.put("block", d.getTitle());
+			json.put("res", d.getResolution());
+			json.put("cid", d.getConventionId());
+		}catch(JSONException ex){
+			ex.printStackTrace();
+		}
+		resp.getWriter().println(json);
+	}
+	
+	// POST is used to submit a comment
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		
+	}
+
+}
